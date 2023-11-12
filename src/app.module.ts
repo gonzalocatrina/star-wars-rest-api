60d0f7d058ba9696626116fc1c7365fc16f4f58a
Gonzalo Catrina <gonzalocatrina@gmail.com>
2023-11-12 10:54:23 -0300

    feat(mongo): db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StarWarsModule } from './star-wars/star-wars.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/star-wars'),StarWarsModule],
   controllers: [AppController],
   providers: [AppService],
 })
