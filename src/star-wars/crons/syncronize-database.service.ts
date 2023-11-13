009c292f6324aa1310aba38661a5e7df4a41694c
Gonzalo Catrina <gonzalocatrina@gmail.com>
2023-11-13 11:28:30 -0300

    docs(syncronize): comments

--- src/star-wars/crons/syncronize-database.service.ts
+++ src/star-wars/crons/syncronize-database.service.ts
@@ -11,27 +11,35 @@ import { Planet } from '../entities/planet.entity';
 @Injectable()
 export class SyncronizeDatabaseService {
   private readonly logger = new Logger(SyncronizeDatabaseService.name);
+
   constructor(
   @InjectModel(Film.name) private filmModel: Model<Film>,
   @InjectModel(Starship.name) private starshipModel: Model<Starship>,
   @InjectModel(People.name) private peopleModel: Model<People>,
   @InjectModel(Planet.name) private planetModel: Model<Planet>) {}
+
   //Cron runs 10 seconds after the app started
   @Cron(new Date(Date.now() + 10 * 1000))
   async handleCron() {
     const initialUrl = process.env.SWAPI_URL;
+
+    // Concurrently updating the database for each entity
     await Promise.all([this.updateDatabase(initialUrl,'films/',this.filmModel, 'title'),
     this.updateDatabase(initialUrl,'starships/',this.starshipModel),
     this.updateDatabase(initialUrl,'people/',this.peopleModel),
     this.updateDatabase(initialUrl,'planets/',this.planetModel)])
     this.logger.debug('Database is up to date');
   }
+
   async updateDatabase(baseUrl: string,paramUrl: string,model:any, uniqueField?:string){
     const results = await this.getAllResults(baseUrl+paramUrl);
     this.logger.debug('Results to create/update from ' + baseUrl+paramUrl + ' is: '+ results.length);
+
+    // Updating the database with the fetched results
     await this.database(results,model, uniqueField)
     this.logger.debug('Updated database from ', baseUrl+paramUrl);
   }
+
   async fetchData(url: string) {
       try {
       const response = await axios.get(url);
@@ -41,6 +49,8 @@ export class SyncronizeDatabaseService {
       throw new Error("Error fetching data from " + url);
       }
   }
+
+  // Recursive function to get all results from a paginated API
   async getAllResults(url: string, allResults = []) {
       try {
       const data = await this.fetchData(url);
@@ -52,10 +62,14 @@ export class SyncronizeDatabaseService {
       throw error;
       }
     }
+
+  // Function to update or create documents in the database
   async database(results:any[], model:Model<any>, uniqueField: string = 'name'){
     await Promise.all(results.map(async (result) => {
       const query = {};
       query[uniqueField] = result[uniqueField];
+
+      // Update if exists, otherwise create a new document
       const itExists = await model.findOneAndUpdate(query, result).exec();
       if (!itExists) {
         const newModel = new model(result);
